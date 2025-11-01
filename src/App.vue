530252afafff7f6626347c68c66e41831996d4d1
suibian <3555460670@qq.com>
2025-11-01 16:13:41 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Home />
   <footer>
     <span>© 2024 Made in <a href="/" target="_blank">{{ userName }}</a></span>
-    <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
+    <a v-if="icpNumber" href="https://icp.gov.moe/" target="_blank">{{ icpNumber }}</a>
     <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
       <span class="police_img"></span> {{ policenumber }}
     </a>
